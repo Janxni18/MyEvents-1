8eb3b32c454f5171af581e50ee6b23d59ab79075
npremajayantha <naveenpremajayantha.gmail.com>
2019-08-10 10:31:20 +0530

    Added user profile and add events

--- app/src/main/java/com/example/myevents/MainActivity.java
+++ app/src/main/java/com/example/myevents/MainActivity.java
@@ -1,64 +1,56 @@
 package com.example.myevents;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import android.view.View;
-
-import androidx.core.view.GravityCompat;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-
-import android.view.MenuItem;
-
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-    private Button btn;
+public class MainActivity extends AppCompatActivity {
+    private Button btnlogin,btnregister,btnevent,btnprofile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
 
 
-        btn = findViewById(R.id.btn_next);
-        btn.setOnClickListener(new View.OnClickListener()
+
+        btnevent = findViewById(R.id.buttonEvents);
+        btnevent.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v){
                 openAdduser();
             }
         });
+
+        btnlogin = findViewById(R.id.buttonLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                openLogin();
+            }
+        });
+        btnregister = findViewById(R.id.buttonRegister);
+        btnregister.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                openRegister();
+            }
+        });
+        btnprofile = findViewById(R.id.buttonAbout_us1);
+        btnprofile.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                openProfile();
+            }
+        });
+
     }
 
     public void openAdduser() {
@@ -67,61 +59,24 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
 
     }
+    public void openLogin() {
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        Intent intent = new Intent(this,login.class);
+        startActivity(intent);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void openRegister() {
+
+        Intent intent = new Intent(this,register.class);
+        startActivity(intent);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void openProfile() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent intent = new Intent(this,userProfile.class);
+        startActivity(intent);
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
